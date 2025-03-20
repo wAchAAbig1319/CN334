6b823eb60e77daea7d1921ead8056958c27fe4cb
wAchAAbig1319 <maiperdnachlove@gmail.com>
2025-03-20 16:18:26 +0700

    Adding new ecommerce app and item page

---
+++ ecommerce/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def ecommerce_index_view(request):
+ '''This function render index page of ecommerce views'''
+ return HttpResponse('Welcome to 6610742238 Matthew Tangjitmotana views!')
+def item_view(request, item_id):
+ context_data = {
+ "item_id": item_id
+ }
+ return render(request, 'index.html',context = context_data)
